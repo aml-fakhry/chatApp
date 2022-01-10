d06c25609fa3b4cd30ead9dc33405a827bd257e9
aml-fakhry <amlfakhry13@gmail.com>
2022-01-10 10:52:13 +0200

    setup app.

---
+++ app.js
@@ -0,0 +1,49 @@
+import express, { json, urlencoded } from 'express';
+import { createServer } from 'http';
+import path, { dirname } from 'path';
+import { Server } from 'socket.io';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+const PORT = 5555;
+
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+// create a route for the app
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+app.get('/ejs', (req, res, next) => {
+  let msgs = [];
+  res.render('index1.ejs', {
+    data: req.body,
+  });
+});
+app.get('/send', (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+let clients = new Map();
+io.on('connection', (socket) => {
+  console.log('new user connected');
+  socket.once('name', (name) => {
+    clients.set(name, socket);
+  });
+
+  socket.on('chat message', (msg) => {
+    let toSocket = clients.get(msg.to);
+    toSocket.emit('chat message', msg.text);
+  });
+});
+
+// make the server listen to requests
+httpServer.listen(PORT, () => {
+  console.log(`Server running at: http://localhost:${PORT}/`);
+});
