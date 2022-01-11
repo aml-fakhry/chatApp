64530566029be6af1dea173185503ae52d171aae
aml-fakhry <amlfakhry13@gmail.com>
2022-01-11 16:13:23 +0200

    add typing.

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ import { createServer } from 'http';
 import path, { dirname } from 'path';
 import { Server } from 'socket.io';
 import { fileURLToPath } from 'url';
+import cors from 'cors';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = 5555;
-
+const PORT = process.env.PORT || 5555;
+app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
@@ -33,6 +34,7 @@ app.get('/send', (req, res) => {
 let clients = new Map();
 io.on('connection', (socket) => {
   console.log('new user connected');
+  console.log(socket.client.conn.server.clientsCount + ' users connected');
   socket.once('name', (name) => {
     clients.set(name, socket);
   });
@@ -49,6 +51,11 @@ io.on('connection', (socket) => {
   socket.on('noTyping', function (data) {
     // send an event to everyone but the person who emitted the typing event to the server
     socket.broadcast.emit('noTyping', data);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+    socket.emit('message', 'user disconnected');
   });
 });
 
