862abbb3163185687acf3763969370265361a025
Aml Fakhri <amlfakhry13@gmail.com>
2022-04-25 16:15:23 +0200

    fix logic.

--- app.js
+++ app.js
@@ -1,29 +1,27 @@
-import express, { json, urlencoded } from 'express';
+import express from 'express';
 import { createServer } from 'http';
 import path, { dirname } from 'path';
 import { Server } from 'socket.io';
 import { fileURLToPath } from 'url';
-import cors from 'cors';
+
+/* This function ensures the correct decodings of percent-encoded characters as well as ensuring a cross-platform valid absolute path string. */
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+/* Creates an Express application. */
 const app = express();
-const PORT = process.env.PORT || 5555;
-app.use(cors());
-app.use(json());
-app.use(urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
 
+/* Setup socket server. */
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
 // create a route for the app
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.get('/', (_req, res) => {
+  res.sendFile(path.join(__dirname, '/views/index.html'));
 });
 
-app.get('/room', (req, res) => {
-  res.sendFile(path.join(__dirname, '/room.html'));
+app.get('/room', (_req, res) => {
+  res.sendFile(path.join(__dirname, '/views/room.html'));
 });
 
 let clients = new Map();
@@ -38,6 +36,7 @@ io.on('connection', (socket) => {
     let toSocket = clients.get(msg.to);
     toSocket.emit('chat message', msg.text);
   });
+
   socket.on('typing', function (data) {
     // send an event to everyone but the person who emitted the typing event to the server
     socket.broadcast.emit('typing', data);
@@ -48,21 +47,15 @@ io.on('connection', (socket) => {
     socket.broadcast.emit('noTyping', data);
   });
 
-  // socket.on('room', function (room) {
-  //   let toSocket = clients.get(msg.to);
-  //   // if (socket.room) toSocket.leave(socket.room);
-
-  //   toSocket.room = room;
-  //   toSocket.join(room);
-  // });
-
   socket.on('disconnect', () => {
     console.log('user disconnected');
     socket.emit('message', 'user disconnected');
   });
 });
 
-// make the server listen to requests
-httpServer.listen(PORT, () => {
-  console.log(`Server running at: http://localhost:${PORT}/`);
+/* make the server listen to requests */
+httpServer.listen(process.env.PORT || 5555, () => {
+  console.log(
+    `Server running at: http://localhost:${process.env.PORT || 5555}/`
+  );
 });
